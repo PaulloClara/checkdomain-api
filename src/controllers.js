8cfaaaa20b9693c689c162a2c8fa66e2ca258c94
paulloclara <paulloclara@gmail.com>
2020-02-28 07:20:10 -0300

    :rocket: Add valid domain verification

--- src/controllers.js
+++ src/controllers.js
@@ -1,12 +1,15 @@
+import isValidDomain from "is-valid-domain";
 import { checkDomain } from "./services.js";
 
 export async function root(request, response) {
   const { domain } = request.params;
 
-  const result = !(await checkDomain(domain));
+  const validDomain = isValidDomain(domain);
+  const free = validDomain ? !(await checkDomain(domain)) : false;
 
   return response.status(200).send({
+    free,
     domain,
-    free: result
+    validDomain
   });
 }
