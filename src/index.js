ca8d1985011b7597806a45735e69108f6a8c72e2
paulloclara <paulloclara@gmail.com>
2020-02-28 07:43:12 -0300

    :rocket: Initial cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import cors from "cors";
 import express from "express";
+
 import routes from "./routes.js";
 
 const server = express();
 
+server.use(cors());
 server.use(routes);
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log(`\n\n\tRodando na porta ${port}\n`);
+  console.log(`\n\n\tServer running on port ${port}\n`);
 });
